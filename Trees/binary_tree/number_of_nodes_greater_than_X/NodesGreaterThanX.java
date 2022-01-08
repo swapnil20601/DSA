d94ed32fa6b4e84fdcaa10e503511f51dc588917
swapnil20601 <swap47233@gmail.com>
2022-01-08 12:06:00 -0600

    Added optimized soln to isBalanced

--- Trees/binary_tree/number_of_nodes_greater_than_X/NodesGreaterThanX.java
+++ Trees/binary_tree/number_of_nodes_greater_than_X/NodesGreaterThanX.java
@@ -4,16 +4,17 @@ import binary_tree.Node;
 
 public class NodesGreaterThanX {
     public static int countNodesGreaterThanX(Node<Integer> root, int x) {
-        if(root == null){
+        if (root == null) {
             return 0;
         }
 
         int leftCount = countNodesGreaterThanX(root.left, x);
         int rightCount = countNodesGreaterThanX(root.right, x);
-        
-        if(root.data > x){
+
+        if (root.data > x) {
             return 1 + leftCount + rightCount;
         } else {
             return leftCount + rightCount;
         }
+    }
 }
